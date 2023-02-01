30c190a4d3f3ec40de482cff8172ce67e26a8848
Denes Adam Dolhay <dadolhay@myseneca.ca>
2023-02-01 18:54:16 -0500

    improvement: Constant name

--- app/lib/lets-encrypt.server.ts
+++ app/lib/lets-encrypt.server.ts
@@ -2,7 +2,7 @@ import acme from "acme-client";
 import type { Client as AcmeClient } from "acme-client";
 import { secrets } from "docker-secret";
 
-const { LETSENCRYPT_ACCOUNT_PRIVATE_KEY_PEM } = secrets ?? {};
+const { LETS_ENCRYPT_ACCOUNT_PRIVATE_KEY_PEM } = secrets ?? {};
 
 class LetsEncrypt {
   #client?: AcmeClient;
@@ -13,12 +13,12 @@ class LetsEncrypt {
 
   initialize = async () => {
     if (process.env.NODE_ENV === "production") {
-      if (!LETSENCRYPT_ACCOUNT_PRIVATE_KEY_PEM)
+      if (!LETS_ENCRYPT_ACCOUNT_PRIVATE_KEY_PEM)
         throw new Error(
-          "The docker secret LETSENCRYPT_ACCOUNT_PRIVATE_KEY_PEM is missing"
+          "The docker secret LETS_ENCRYPT_ACCOUNT_PRIVATE_KEY_PEM is missing"
         );
 
-      this.#accountKey = LETSENCRYPT_ACCOUNT_PRIVATE_KEY_PEM;
+      this.#accountKey = LETS_ENCRYPT_ACCOUNT_PRIVATE_KEY_PEM;
       this.#directoryUrl = acme.directory.letsencrypt.production;
     } else {
       // For testing and local development, let's use an ad-hoc generated key
