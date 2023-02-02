b51beda8ff00e3923c98d03ad319eb80261fddb1
stefanaz2 <sfrunza@seneca.ca>
2023-02-02 10:24:23 -0500

    Adding login and logout as starchart user from initial seed
    changed login button style
    
    slight changes to login and logout
    
    changed form to Form and imported Args as type
    
    Put types under other imports

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,12 +1,31 @@
-import { Heading, Text } from '@chakra-ui/react';
+import { Heading, Text, Button } from '@chakra-ui/react';
+import { json } from '@remix-run/node';
+import { Form, useLoaderData } from '@remix-run/react';
+import { requireUsername } from '~/session.server';
+
+import type { LoaderArgs } from '@remix-run/node';
+
+export const loader = async ({ request }: LoaderArgs) => {
+  const username = await requireUsername(request);
+
+  return json({ username });
+};
 
 export default function Index() {
+  const data = useLoaderData<typeof loader>();
+
   return (
     <main>
       <Heading as="h1" size="3xl" noOfLines={1}>
         Starchart
       </Heading>
       <Text fontSize="xl">Start making your own custom domains and certificates today!</Text>
+      <Text>Welcome {data.username}</Text>
+      <Form action="/logout" method="post">
+        <Button type="submit" colorScheme="red">
+          Logout
+        </Button>
+      </Form>
     </main>
   );
 }
---
+++ app/routes/logout.tsx
@@ -0,0 +1,13 @@
+import { redirect } from "@remix-run/node";
+
+import type { ActionArgs } from "@remix-run/node";
+
+import { logout } from "~/session.server";
+
+export const action = async ({ request }: ActionArgs) => {
+  return logout(request);
+};
+
+export const loader = async () => {
+  return redirect("/");
+};
