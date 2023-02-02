d0e7abd698608f48602e8537463774e88f5046d1
stefanaz2 <sfrunza@seneca.ca>
2023-02-02 16:50:49 -0500

    fixing imports in logout

--- app/routes/logout.tsx
+++ app/routes/logout.tsx
@@ -1,8 +1,9 @@
 import { redirect } from "@remix-run/node";
+import { logout } from "~/session.server";
 
 import type { ActionArgs } from "@remix-run/node";
 
-import { logout } from "~/session.server";
+
 
 export const action = async ({ request }: ActionArgs) => {
   return logout(request);
