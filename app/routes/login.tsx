17bb3e470ca03006e31519fcb8cef648dc94853b
sfrunza13 <77400826+sfrunza13@users.noreply.github.com>
2023-02-02 23:12:35 -0500

    Adding login and logout as starchart user from initial seed (#88)
    * Adding login and logout as starchart user from initial seed
    
    * Prettiered
    
    ---------
    
    Co-authored-by: stefanaz2 <sfrunza@seneca.ca>

---
+++ app/routes/login.tsx
@@ -0,0 +1,26 @@
+import { Heading, Text, Button } from '@chakra-ui/react';
+import { Form } from '@remix-run/react';
+import { createUserSession } from '~/session.server';
+
+import type { ActionArgs } from '@remix-run/node';
+
+export const action = async ({ request }: ActionArgs) => {
+  return createUserSession({
+    request: request,
+    username: 'starchartdev',
+    remember: false,
+    redirectTo: '/',
+  });
+};
+
+export default function Login() {
+  return (
+    <Form method="post">
+      <Heading>Login</Heading>
+      <Text>You will be logged in (a session will be created for you)</Text>
+      <Button type="submit" colorScheme="red">
+        Login
+      </Button>
+    </Form>
+  );
+}
