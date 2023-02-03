07a6d729d198f74b5ed9f48006042168e27f9d8e
David Humphrey <david.andrew.humphrey@gmail.com>
2023-02-03 17:54:35 -0500

    Switch from Pino to Winston for logging

--- app/lib/logger.server.ts
+++ app/lib/logger.server.ts
@@ -1,15 +1,18 @@
-import pino from 'pino';
-import type { LoggerOptions } from 'pino';
+import winston from 'winston';
 
-const pinoOptions: LoggerOptions = { level: process.env.LOG_LEVEL || 'info' };
-// Pretty debug logging
-if (pinoOptions.level === 'debug') {
-  pinoOptions.transport = {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-    },
-  };
-}
+const { combine, json, simple, colorize, errors, timestamp } = winston.format;
 
-export default pino(pinoOptions);
+const logger = winston.createLogger({
+  // Default to info level, but use whatever's defined in the env
+  level: process.env.LOG_LEVEL?.toLowerCase() || 'info',
+  // In development, use a simplified output, expand errors, use colour.
+  // Use JSON with timestamps in production
+  format:
+    process.env.NODE_ENV === 'production'
+      ? combine(timestamp(), json())
+      : combine(colorize(), errors({ stack: true }), simple()),
+  // Log to stdout/stderr
+  transports: [new winston.transports.Console()],
+});
+
+export default logger;
