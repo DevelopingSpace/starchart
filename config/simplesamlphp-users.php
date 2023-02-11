86b49a2f900d5b053d788bc8aece273c0da9022a
stefanaz2 <sfrunza@seneca.ca>
2023-02-11 14:45:13 -0500

    Changed some .com to .cas

--- config/simplesamlphp-users.php
+++ config/simplesamlphp-users.php
@@ -20,7 +20,7 @@ $config = array(
               and need to simulate here.
             */
             'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress' => 'user1@myseneca.ca',
-            'email' => 'user1@example.com',
+            'email' => 'user1@example.ca',
             'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname' => 'Johannes',
             'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname' => 'Kepler',
             'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name'=> 'user1@myseneca.ca',
@@ -30,19 +30,19 @@ $config = array(
         'user2:user2pass' => array(
             'uid' => array('2'),
             'eduPersonAffiliation' => array('group2'),
-            'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress' => 'user2@myseneca.com',
-            'email' => 'user2@example.com',
+            'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress' => 'user2@myseneca.ca',
+            'email' => 'user2@example.ca',
             'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname' => 'Galileo',
             'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname' => 'Galilei',
-            'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name'=> 'user2@myseneca.com',
+            'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name'=> 'user2@myseneca.ca',
             'sAMAccountName'=> 'user2',
             'http://schemas.microsoft.com/identity/claims/displayname' => 'Galileo Galilei',
         ),
         'lippersheyh:telescope' => array(
           'uid' => array('2'),
           'eduPersonAffiliation' => array('group2'),
-          'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress' => 'hansolo@myseneca.com',
-          'email' => 'hans-lippershey@example.com',
+          'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress' => 'hansolo@myseneca.ca',
+          'email' => 'hansolo@myseneca.ca',
           'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname' => 'Hans',
           'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname' => 'Lippershey',
           'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name'=> 'hansolo@myseneca.ca',
