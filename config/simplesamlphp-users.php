9a9c51bd42259d01695177a4c79cb2e8c964727d
sfrunza13 <77400826+sfrunza13@users.noreply.github.com>
2023-02-12 11:47:03 -0500

    Getting ready for IDP + SP work (#174)
    * Small PR to add Samlify and the Simple SAML IDP exactly as it is in telescope, not yet configured to work together
    
    * Added claims for name and sAMAccountname
    
    * I had a few double quotes in there I swapped for singles
    
    * changed first 2 accounts to be user1 and user2 for ease of use, lower case and same email used for both name and email
    
    * Changed some .com to .cas
    
    * example domain to seneca domain
    
    ---------
    
    Co-authored-by: stefanaz2 <sfrunza@seneca.ca>

---
+++ config/simplesamlphp-users.php
@@ -0,0 +1,55 @@
+<?php
+/*
+  Specify user info for https://hub.docker.com/r/kristophjunge/test-saml-idp/ in the same format
+  as our Seneca IdP will give it.  This is modeled on the original config here:
+  https://github.com/kristophjunge/docker-test-saml-idp/blob/master/config/simplesamlphp/authsources.php
+*/
+$config = array(
+
+    'admin' => array(
+        'core:AdminPassword',
+    ),
+
+    'example-userpass' => array(
+        'exampleauth:UserPass',
+        'user1:user1pass' => array(
+            'uid' => array('1'),
+            'eduPersonAffiliation' => array('group1'),
+            /*
+              NOTE: we add a bunch of claims that we expect to get back from Seneca,
+              and need to simulate here.
+            */
+            'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress' => 'user1@myseneca.ca',
+            'email' => 'user1@myseneca.ca',
+            'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname' => 'Johannes',
+            'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname' => 'Kepler',
+            'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name'=> 'user1@myseneca.ca',
+            'sAMAccountName'=> 'user1', 
+            'http://schemas.microsoft.com/identity/claims/displayname' => 'Johannes Kepler',  
+        ),
+        'user2:user2pass' => array(
+            'uid' => array('2'),
+            'eduPersonAffiliation' => array('group2'),
+            'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress' => 'user2@myseneca.ca',
+            'email' => 'user2@myseneca.ca',
+            'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname' => 'Galileo',
+            'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname' => 'Galilei',
+            'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name'=> 'user2@myseneca.ca',
+            'sAMAccountName'=> 'user2',
+            'http://schemas.microsoft.com/identity/claims/displayname' => 'Galileo Galilei',
+        ),
+        'lippersheyh:telescope' => array(
+          'uid' => array('2'),
+          'eduPersonAffiliation' => array('group2'),
+          'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress' => 'hansolo@myseneca.ca',
+          'email' => 'hansolo@myseneca.ca',
+          'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname' => 'Hans',
+          'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname' => 'Lippershey',
+          'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name'=> 'hansolo@myseneca.ca',
+          'sAMAccountName'=> 'hansolo',
+          'http://schemas.microsoft.com/identity/claims/displayname' => 'Hans Lippershey',
+          
+      ),
+    ),
+
+);
