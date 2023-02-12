539412c7f0798590b8eeb71ce650b426e6167e10
Myrfion <Levtsuntimur@gmail.com>
2023-02-11 21:03:24 -0500

    Add domain table to domains list page
    ### Overview
    Added `DomainsTable` component, that takes `domains` and `onAction` props and renders Chakra UI table with domains. It also triggers `onAction` callback with different actions. Only `EDIT` action handling is implemented for now, since I need more back-end stuff updates to implement other actions handling. I also added mock file with some domains, just to have something to render for now.
    
    Resolves #137

--- app/routes/__index/domains/index.tsx
+++ app/routes/__index/domains/index.tsx
@@ -1,11 +1,32 @@
-import { Heading } from '@chakra-ui/react';
+import { Container, Heading } from '@chakra-ui/react';
+import { useNavigate } from '@remix-run/react';
+import type { DomainsTableAction } from '~/components/domains-table';
+import DomainsTable from '~/components/domains-table';
+import DOMAINS_MOCK from '~/mocks/domains';
 
 export default function DomainsIndexRoute() {
+  const navigate = useNavigate();
+
+  function onDomainAction(domainID: number, action: DomainsTableAction) {
+    switch (action) {
+      case 'EDIT':
+        navigate(domainID.toString());
+        break;
+      case 'DELETE':
+        // TODO: implement delete functionaty
+        break;
+      case 'RENEW':
+        // TODO: implement renew functionaty
+        break;
+    }
+  }
+
   return (
-    <div>
-      <Heading as="h1" size="3xl">
+    <Container maxW="contianer.xl">
+      <Heading as="h1" size="2xl" mt="8">
         Domains List
       </Heading>
-    </div>
+      <DomainsTable domains={DOMAINS_MOCK} onAction={onDomainAction} />
+    </Container>
   );
 }
