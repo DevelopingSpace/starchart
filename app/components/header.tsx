e0eb3271132b98927e8fdb347bb50044f0fdb302
Ririio <32577022+Ririio@users.noreply.github.com>
2023-02-14 19:51:04 -0500

    making header responsinve (#212)
    adding margin for certifacate and domain

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -9,8 +9,11 @@ import {
   MenuList,
   MenuItem,
   IconButton,
+  Show,
+  Hide,
+  Button,
 } from '@chakra-ui/react';
-import { TriangleUpIcon, LockIcon } from '@chakra-ui/icons';
+import { TriangleUpIcon, LockIcon, HamburgerIcon } from '@chakra-ui/icons';
 import { Link, useFetcher } from '@remix-run/react';
 
 import { useUser } from '~/utils';
@@ -18,43 +21,74 @@ import { useUser } from '~/utils';
 export default function Header() {
   const user = useUser();
   const fetcher = useFetcher();
+
   return (
     <Flex
       as="header"
       backgroundColor="brand.500"
       height="auto"
+      width="100%"
       alignItems="center"
       shadow="2xl"
       fontSize="18"
       paddingY="2"
+      paddingX={{ base: '1', md: '10' }}
+      justifyContent="space-between"
     >
-      <HStack width="50%" color="white" justifyContent="flex-start" gap="10" marginLeft="10">
-        <Link to={{ pathname: '/domains' }}>
-          <Flex alignItems="center">
-            <TriangleUpIcon marginRight="2" />
-            <Text>Domains</Text>
-          </Flex>
-        </Link>
+      <Hide below="md">
+        <HStack color="white" justifyContent="flex-start">
+          <Link to={{ pathname: '/domains' }}>
+            <Flex alignItems="center">
+              <TriangleUpIcon marginRight="2" />
+              <Text>Domains</Text>
+            </Flex>
+          </Link>
 
-        <Link to={{ pathname: '/certificate' }}>
-          <Flex alignItems="center">
-            <LockIcon marginRight="2" />
-            <Text>Certificate</Text>
-          </Flex>
-        </Link>
-      </HStack>
-      <Heading as="h1" size="xl" color="white">
+          <Link to={{ pathname: '/certificate' }}>
+            <Flex alignItems="center" marginLeft="5">
+              <LockIcon marginRight="2" />
+              <Text>Certificate</Text>
+            </Flex>
+          </Link>
+        </HStack>
+      </Hide>
+      <Show below="md">
+        <Menu>
+          <MenuButton
+            as={Button}
+            rightIcon={<HamburgerIcon />}
+            size="auto"
+            style={{ backgroundColor: 'transparent' }}
+          />
+          <MenuList>
+            <MenuItem>
+              <Link to={{ pathname: '/domains' }}>
+                <Flex alignItems="center">
+                  <TriangleUpIcon marginRight="2" />
+                  <Text>Domains</Text>
+                </Flex>
+              </Link>
+            </MenuItem>
+            <MenuItem>
+              <Link to={{ pathname: '/certificate' }}>
+                <Flex alignItems="center">
+                  <LockIcon marginRight="2" />
+                  <Text>Certificate</Text>
+                </Flex>
+              </Link>
+            </MenuItem>
+          </MenuList>
+        </Menu>
+      </Show>
+
+      <Heading as="h1" size={{ base: 'md', sm: 'xl' }} color="white">
         My.Custom.Domain
       </Heading>
-      <Flex
-        width="50%"
-        justifyContent="flex-end"
-        alignItems="center"
-        color="white"
-        gap="5"
-        marginRight="10"
-      >
-        <Text>{user.username}</Text>
+      <Flex justifyContent="flex-end" alignItems="center" color="white" gap="5">
+        <Hide below="md">
+          <Text>{user.username}</Text>
+        </Hide>
+
         <Menu>
           <MenuButton
             as={IconButton}
