a518cc3a8f10964d3bcfa6f74926139487a21260
Eakampreet <eakam1007@gmail.com>
2023-02-16 19:05:01 -0500

    Update domain creation page html

--- app/routes/__index/domains/new.tsx
+++ app/routes/__index/domains/new.tsx
@@ -1,11 +1,17 @@
-import { Heading } from '@chakra-ui/react';
+import { Container, Heading, Text } from '@chakra-ui/react';
+import DomainForm from '~/components/domain-form';
 
 export default function NewDomainRoute() {
   return (
-    <div>
-      <Heading as="h1" size="3xl">
-        New Domain Form
+    <Container maxW="container.xl">
+      <Heading as="h1" size="xl" mt="8">
+        Create new domain
       </Heading>
-    </div>
+      <Text maxW="lg" mb="3" mt="2">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+        been the industry's standard dummy text ever since the 1500s
+      </Text>
+      <DomainForm />
+    </Container>
   );
 }
