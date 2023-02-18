8422afe0d120c62c318a856ad7d458663ca9dc1b
Eakampreet <eakam1007@gmail.com>
2023-02-18 18:08:47 -0500

    Update new domain page with a simple create action

--- app/routes/__index/domains/new.tsx
+++ app/routes/__index/domains/new.tsx
@@ -1,9 +1,44 @@
 import { Container, Heading, Text } from '@chakra-ui/react';
+import { json, redirect } from '@remix-run/node';
+
 import DomainForm from '~/components/domain-form';
+import { createRecord } from '~/models/record.server';
+import { getUsername } from '~/session.server';
+
+import type { ActionArgs } from '@remix-run/node';
+import { RecordType } from '@prisma/client';
+
+export const action = async ({ request }: ActionArgs) => {
+  const form = await request.formData();
+  const name = form.get('name')?.toString();
+  const type = form.get('type')?.toString();
+  const value = form.get('value')?.toString();
+  const ports = form.get('ports')?.toString();
+  const course = form.get('course')?.toString();
+  const description = form.get('description')?.toString();
+  const username = await getUsername(request);
+
+  if (!username || !name || !value || !type || !(type in RecordType)) {
+    return json({ status: 'error' }, { status: 400 });
+  }
+
+  const record = await createRecord(
+    username,
+    name,
+    RecordType[type as keyof typeof RecordType],
+    value,
+    'pending',
+    description,
+    course,
+    ports
+  );
+
+  return redirect(`/domains/${record.id}`);
+};
 
 export default function NewDomainRoute() {
   return (
-    <Container maxW="container.xl">
+    <Container maxW="container.xl" ml={[null, null, '10vw']}>
       <Heading as="h1" size="xl" mt="8">
         Create new domain
       </Heading>
