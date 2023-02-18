270e1977ad18004750177c0032ce3a8b952cb814
Eakampreet <eakam1007@gmail.com>
2023-02-18 18:08:15 -0500

    Add form elements

--- app/components/domain-form.tsx
+++ app/components/domain-form.tsx
@@ -1,15 +1,26 @@
-import { Input, Select, VStack } from '@chakra-ui/react';
+import { AddIcon } from '@chakra-ui/icons';
+import { Button, Input, Select, Textarea, VStack } from '@chakra-ui/react';
+import { Form } from '@remix-run/react';
 
 export default function DomainForm() {
   return (
-    <VStack maxW="lg" spacing="5">
-      <Input placeholder="Domain Name" />
-      <Select placeholder="Types">
-        <option value="cname">CNAME</option>
-      </Select>
-      <Input placeholder="Value" />
-      <Input placeholder="Ports" />
-      <Input placeholder="Course" />
-    </VStack>
+    <Form method="post">
+      <VStack maxW="lg" spacing="5">
+        <Input placeholder="Domain Name" name="name" />
+        <Select placeholder="Type" name="type">
+          <option value="A">A</option>
+          <option value="AAAA">AAAA</option>
+          <option value="CNAME">CNAME</option>
+          <option value="TXT">TXT</option>
+        </Select>
+        <Input placeholder="Value" name="value" />
+        <Input placeholder="Ports" name="ports" />
+        <Input placeholder="Course" name="course" />
+        <Textarea placeholder="Description" rows={10} name="description" />
+      </VStack>
+      <Button type="submit" mt="6" rightIcon={<AddIcon boxSize={3} />}>
+        Create
+      </Button>
+    </Form>
   );
 }
