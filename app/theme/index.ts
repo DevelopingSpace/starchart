2c2cb20495c57826cacde1227d8ce0572e1b2334
Eakampreet <eakam1007@gmail.com>
2023-02-18 23:44:58 -0500

    Add common domain form styles to theme

--- app/theme/index.ts
+++ app/theme/index.ts
@@ -1,6 +1,22 @@
 import { extendTheme, withDefaultColorScheme } from '@chakra-ui/react';
 import colors from './colors';
 
-const theme = extendTheme({ colors }, withDefaultColorScheme({ colorScheme: 'brand' }));
+const theme = extendTheme(
+  {
+    colors,
+    styles: {
+      global: {
+        '.domain-form': {
+          // InfoIcon
+          '.chakra-icon': {
+            color: '#d9d9d9',
+            fontSize: 'xl',
+          },
+        },
+      },
+    },
+  },
+  withDefaultColorScheme({ colorScheme: 'brand' })
+);
 
 export default theme;
