e9089ac6e6227b8dcb4845be4a1fa02662c3aed6
stefanaz2 <sfrunza@seneca.ca>
2023-03-06 17:31:27 -0500

    cleaned up obvious linting errors and made use of the relative path we declare we use in config

--- test/e2e/login.spec.ts
+++ test/e2e/login.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 test.describe('authenticated', () => {
   test('and redirect to /', async ({ page }) => {
-    await page.goto('http://localhost:8080/login?redirectTo=%2F');
+    await page.goto('/login?redirectTo=%2F');
     await page.getByRole('button', { name: 'Sign In' }).click();
     await page.getByLabel('Username').click();
     await page.getByLabel('Username').fill('user1');
@@ -9,8 +9,8 @@ test.describe('authenticated', () => {
     await page.getByLabel('Password').fill('user1pass');
     await page.getByLabel('Password').press('Enter');
 
-    const title = await page.getByRole('heading', { name: 'Starchart' });
-    const userText = await page.getByText('user1', { exact: true });
+    const title = page.getByRole('heading', { name: 'Starchart' });
+    const userText = page.getByText('user1', { exact: true });
 
     await expect(page).toHaveURL(/localhost:8080/);
     await expect(title).toContainText('Starchart');
@@ -18,7 +18,7 @@ test.describe('authenticated', () => {
   });
 
   test('and redirect to /dev', async ({ page }) => {
-    await page.goto('http://localhost:8080/login?redirectTo=%2Fdev');
+    await page.goto('/login?redirectTo=%2Fdev');
     await page.getByRole('button', { name: 'Sign In' }).click();
     await page.getByLabel('Username').click();
     await page.getByLabel('Username').fill('user1');
@@ -26,12 +26,12 @@ test.describe('authenticated', () => {
     await page.getByLabel('Password').fill('user1pass');
     await page.getByLabel('Password').press('Enter');
 
-    const title = await page.getByRole('heading', { name: 'Starchart - Dev' });
-    const userText = await page.getByText(
+    const title = page.getByRole('heading', { name: 'Starchart - Dev' });
+    const userText = page.getByText(
       'Welcome user1! NOTE: this page is only used for development testing.'
     );
 
-    await expect(page).toHaveURL(/localhost:8080\/dev/);
+    await expect(page).toHaveURL(/\/dev/);
     await expect(title).toContainText('Starchart');
     await expect(userText).toContainText('user1');
   });
