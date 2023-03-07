58f7c422e4cd71dc11092c2f6cec7573178dab2a
stefanaz2 <sfrunza@seneca.ca>
2023-03-07 12:53:14 -0500

    id so I can grab it

--- test/e2e/login.spec.ts
+++ test/e2e/login.spec.ts
@@ -7,7 +7,7 @@ test('Login with User 1', async ({ page }) => {
   await page.getByLabel('Password').fill('user1pass');
   await page.getByRole('button', { name: 'Login' }).click();
   await page.waitForURL('http://localhost:8080');
-  const locator = page.getByText('user1', { exact: true });
+  const locator = page.locator('#header-user');
   const starchartHeading = page.getByRole('heading', { name: 'Starchart' });
   await expect(locator).toContainText('user1');
   await expect(starchartHeading).toContainText('Starchart');
@@ -20,7 +20,7 @@ test('Login with User 1 dev redirect', async ({ page }) => {
   await page.getByLabel('Password').fill('user1pass');
   await page.getByRole('button', { name: 'Login' }).click();
   await page.waitForURL('http://localhost:8080/domains');
-  const locator1 = page.getByText('user1');
+  const locator1 = page.locator('#header-user');
   const userInNav = page.getByRole('heading', { name: 'My.Custom.Domain' });
   const domainHeading = page.getByRole('heading', { name: 'Domains' });
   await expect(locator1).toContainText('user1');
