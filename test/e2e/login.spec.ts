26c19c93616bf3ac43b146887e0f8c5637d0fd92
stefanaz2 <sfrunza@seneca.ca>
2023-03-06 20:17:45 -0500

    First test

--- test/e2e/login.spec.ts
+++ test/e2e/login.spec.ts
@@ -1,40 +1,15 @@
 import { test, expect } from '@playwright/test';
-test.describe('authenticated', () => {
-  test('and redirect to /', async ({ page }) => {
-    await page.goto('/login?redirectTo=%2F');
-    await page.getByRole('button', { name: 'Sign In' }).click();
-    await page.getByLabel('Username').click();
-    await page.getByLabel('Username').fill('user1');
-    await page.getByLabel('Password').click();
-    await page.getByLabel('Password').fill('user1pass');
-    await page.getByLabel('Password').press('Enter');
 
-    const title = page.getByRole('heading', { name: 'Starchart' });
-    const userText = page.getByText('user1', { exact: true });
-
-    await expect(page).toHaveURL(/localhost:8080/);
-    await expect(title).toContainText('Starchart');
-    await expect(userText).toContainText('user1');
-  });
-
-  test('and redirect to /dev', async ({ page }) => {
-    await page.goto('/login?redirectTo=%2Fdev');
-    await page.getByRole('button', { name: 'Sign In' }).click();
-    await page.getByLabel('Username').click();
-    await page.getByLabel('Username').fill('user1');
-    await page.getByLabel('Password').click();
-    await page.getByLabel('Password').fill('user1pass');
-    await page.getByLabel('Password').press('Enter');
-
-    const title = page.getByRole('heading', { name: 'Starchart - Dev' });
-    const userText = page.getByText(
-      'Welcome user1! NOTE: this page is only used for development testing.'
-    );
-
-    await expect(page).toHaveURL(/\/dev/);
-    await expect(title).toContainText('Starchart');
-    await expect(userText).toContainText('user1');
-  });
+test('Login w User 1', async ({ page }) => {
+  await page.goto('/login?redirectTo=%2F');
+  await page.getByRole('button', { name: 'Sign In' }).click();
+  await page.getByLabel('Username').click();
+  await page.getByLabel('Username').fill('user1');
+  await page.getByLabel('Password').click();
+  await page.getByLabel('Password').fill('user1pass');
+  await page.getByRole('button', { name: 'Login' }).click();
+  const locator = await page.getByText('user1', { exact: true });
+  const starchartHeading = await page.getByRole('heading', { name: 'Starchart' });
+  await expect(locator).toHaveCount(1);
+  await expect(starchartHeading).toHaveCount(1);
 });
-
-test.describe('authentication API calls', () => {});
