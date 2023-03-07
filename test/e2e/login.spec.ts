33cc64440b4e2c6b348dec065a2c5753dd32c816
stefanaz2 <sfrunza@seneca.ca>
2023-03-07 11:21:22 -0500

    Domains instead

--- test/e2e/login.spec.ts
+++ test/e2e/login.spec.ts
@@ -14,20 +14,16 @@ test('Login with User 1', async ({ page }) => {
 });
 
 test('Login with User 1 dev redirect', async ({ page }) => {
-  await page.goto('/login?redirectTo=%2Fdev');
+  await page.goto('/login?redirectTo=%2Fdomains');
   await page.getByRole('button', { name: 'Sign In' }).click();
   await page.getByLabel('Username').fill('user1');
   await page.getByLabel('Password').fill('user1pass');
   await page.getByRole('button', { name: 'Login' }).click();
-  await page.waitForURL('http://localhost:8080/dev');
-  const locator1 = page.getByText(
-    'Welcome user1! NOTE: this page is only used for development testing.'
-  );
-  const starchartHeading1 = page.getByRole('heading', { name: 'Starchart - Dev' });
-  const devlink1 = page.getByRole('button', { name: 'Request DNS Record' });
-  const devlink2 = page.getByRole('button', { name: 'Request Certificate' });
+  await page.waitForURL('http://localhost:8080/domains');
+  const locator1 = page.getByText('user1');
+  const userInNav = page.getByRole('heading', { name: 'My.Custom.Domain' });
+  const domainHeading = page.getByRole('heading', { name: 'Domains' });
   await expect(locator1).toContainText('user1');
-  await expect(starchartHeading1).toContainText('Starchart');
-  await expect(devlink1).toContainText('DNS');
-  await expect(devlink2).toContainText('Certificate');
+  await expect(userInNav).toContainText('My.Custom.Domain');
+  await expect(domainHeading).toContainText('Domains');
 });
