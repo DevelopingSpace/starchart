e666d25b92889fdc7802ea396c33cd5f0b4bfce6
Eakampreet <eakam1007@gmail.com>
2023-03-10 04:54:04 -0500

    Revert back to old test logic

--- test/e2e/new-dns-record.spec.ts
+++ test/e2e/new-dns-record.spec.ts
@@ -12,29 +12,29 @@ test.describe('authenticated as user', () => {
   test.use({ storageState: 'test/e2e/.auth/user.json' });
 
   test('redirects to edit dns record page when required fields are filled', async ({ page }) => {
-    await page.goto('/domains/new', { waitUntil: 'domcontentloaded' });
+    await page.goto('/domains/new');
     await page.getByLabel('Record Name*').fill('test');
     await page.getByRole('combobox', { name: 'Type' }).selectOption('A');
     await page.getByLabel('Value*').fill('test');
-    await page.getByLabel('Value*').press('Enter');
+    await page.getByRole('button', { name: 'Create' }).click();
     await expect(page).toHaveURL(/.*domains\/[0-9]*/);
   });
 
   test('redirects to edit dns record page when all fields are filled', async ({ page }) => {
-    await page.goto('/domains/new', { waitUntil: 'domcontentloaded' });
+    await page.goto('/domains/new');
     await page.getByLabel('Record Name*').fill('test');
     await page.getByRole('combobox', { name: 'Type' }).selectOption('A');
     await page.getByLabel('Value*').fill('test');
     await page.getByLabel('Ports').fill('port1, port2');
     await page.getByLabel('Course').fill('test course');
     await page.getByLabel('Description').fill('test description');
-    await page.getByLabel('Description').press('Enter');
+    await page.getByRole('button', { name: 'Create' }).click();
     await expect(page).toHaveURL(/.*domains\/[0-9]*/);
   });
 
   test('does not create dns record if required fields are empty', async ({ page }) => {
-    await page.goto('/domains/new', { waitUntil: 'domcontentloaded' });
-    await page.getByLabel('Record Name*').press('Enter');
+    await page.goto('/domains/new');
+    await page.getByRole('button', { name: 'Create' }).click();
     await expect(page).toHaveURL(/.*domains\/new/);
   });
 });
