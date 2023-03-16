477bd5ef3881d1d79d632edc950e0908447c6dd3
cchu42 <cchu42@myseneca.ca>
2023-03-16 17:28:38 -0400

    * Add SQL setup file for database * Change volume folder structure * Adjust connection string for .env.example

---
+++ mysql/init/01-databases.sql
@@ -0,0 +1,7 @@
+-- create regular and test databases
+CREATE DATABASE IF NOT EXISTS `startchart`;
+CREATE DATABASE IF NOT EXISTS `starchart_test`;
+
+-- create root user and grant rights
+CREATE USER 'root'@'localhost' IDENTIFIED BY 'root_password';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
