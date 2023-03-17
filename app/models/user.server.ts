b0bb7b9abf6458c1ea0aff9eb9d2929aebc97d4c
Eakampreet <eakam1007@gmail.com>
2023-03-17 06:06:08 -0400

    Add methods to deactivate and delete a user

--- app/models/user.server.ts
+++ app/models/user.server.ts
@@ -47,6 +47,17 @@ export async function createUser(
   });
 }
 
+export async function deactivateUserByUsername(username: PrismaUser['username']) {
+  return prisma.user.update({
+    where: { username },
+    data: { deactivated: true },
+  });
+}
+
+export async function deleteUserByUsername(username: PrismaUser['username']) {
+  return prisma.user.delete({ where: { username } });
+}
+
 export async function isStudent(username: PrismaUser['username']) {
   const { group } = await prisma.user.findUniqueOrThrow({ where: { username } });
   // The group will have -dev in it on staging but not on prod
