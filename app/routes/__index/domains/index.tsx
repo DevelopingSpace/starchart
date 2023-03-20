c41f712e1e945de11060f6a01511681231c230b8
Myrfion <Levtsuntimur@gmail.com>
2023-03-19 21:01:14 -0400

    Update error code

--- app/routes/__index/domains/index.tsx
+++ app/routes/__index/domains/index.tsx
@@ -53,7 +53,7 @@ export const action = async ({ request }: ActionArgs) => {
 
   if (record.status !== 'active') {
     throw new Response('Record is not active, action forbidden', {
-      status: 403,
+      status: 409,
     });
   }
 
