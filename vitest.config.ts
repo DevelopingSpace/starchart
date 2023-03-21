71693a8d27cf24976d004023ed2c763469ce808a
Eakampreet <eakam1007@gmail.com>
2023-03-21 18:43:42 -0400

    Remove redundant includes and update other vitest config settings

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,12 +9,8 @@ export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   test: {
     globals: true,
-    environment: 'happy-dom',
     setupFiles: ['./test/unit/setup-test-env.ts'],
-    include: [
-      './app/**/unit/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}',
-      './test/unit/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}',
-    ],
-    watchExclude: ['.*\\/node_modules\\/.*', '.*\\/build\\/.*', '.*\\/postgres-data\\/.*'],
+    include: ['./test/unit/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    watchExclude: ['.*\\/node_modules\\/.*', '.*\\/build\\/.*', '.*\\/mysql-data\\/.*'],
   },
 });
