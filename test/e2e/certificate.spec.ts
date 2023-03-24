a4fab841a3f7bb2474fafde231be6383439f0c16
Ririio <xRiriio@gmail.com>
2023-03-24 18:20:18 -0400

    resolving issue with e2e test

--- test/e2e/certificate.spec.ts
+++ test/e2e/certificate.spec.ts
@@ -1,21 +1,29 @@
 import { test, expect } from '@playwright/test';
+
 import { loggedInAsUser } from './utils';
+import {
+  getCertificateStatusByUsername,
+  createCertificate,
+  deleteAllByUsername,
+  updateCertificateById,
+} from '../../app/models/certificate.server';
 
 test.describe('Certificate Page', () => {
   loggedInAsUser();
 
   test.beforeEach(async ({ page }) => {
-    await page.goto('/certificate');
-  });
+    test('Request a Certificate Page', async ({ page }) => {
+      await page.goto('/certificate');
+      await deleteAllByUsername('user1');
+    });
 
-  test('Request a Certificate', async ({ page }) => {
     const titleHeader = page.getByRole('heading', { name: 'Certificate' });
-    const domainName = page.getByText('user1.starchart.com');
+    await page.waitForSelector('text=user1.starchart.com');
 
-    await expect(domainName).toContainText('user1.starchart.com');
-    await expect(titleHeader).toContainText('Certificate');
+    await titleHeader.waitFor();
 
     await page.getByRole('button', { name: 'Request a Certificate' }).click();
+    createCertificate({ username: 'user1', domain: 'user1.starchart.com' });
 
     const loadingPageText = page
       .locator('div')
@@ -24,35 +32,40 @@ test.describe('Certificate Page', () => {
       })
       .nth(1);
 
-    await loadingPageText.waitFor();
+    loadingPageText.waitFor();
+
+    const certificate = await getCertificateStatusByUsername('user1');
+    await updateCertificateById(certificate.id, {
+      status: 'issued',
+      validFrom: new Date(),
+      validTo: new Date(),
+    });
+
+    //Copy Key Toast Text
+    await page.getByRole('button', { name: 'Copy Public Key' }).click();
+    await page.getByRole('button', { name: 'Copy Private Key' }).click();
+
+    const publicCopyToastText = page.getByText('Public Key was copied to the clipboard');
+    const privateCopyToastText = page.getByText('Private Key was copied to the clipboard');
+
+    await publicCopyToastText.waitFor();
+    await privateCopyToastText.waitFor();
+
+    //Download Key Toast Text
+    await page.getByRole('button', { name: 'Download Public Key' }).click();
+    await page.getByRole('button', { name: 'Download Private Key' }).click();
+
+    const publicDownloadToastText = page.getByText('Public Key is Downloaded');
+    const privateDownloadToastText = page.getByText('Private Key is Downloaded');
+
+    await publicDownloadToastText.waitFor();
+    await privateDownloadToastText.waitFor();
+
+    //Text Titles
+    const publicKey = page.getByRole('heading', { name: 'Public Key' });
+    const privateKey = page.getByRole('heading', { name: 'Private Key' });
+
+    await expect(publicKey).toContainText('Public Key');
+    await expect(privateKey).toContainText('Private Key');
   });
-
-  // test('Dispaly Certificate', async ({ page }) => {
-  //   //Copy Key Toast Text
-  //   await page.getByRole('button', { name: 'Copy Public Key' }).click();
-  //   await page.getByRole('button', { name: 'Copy Private Key' }).click();
-
-  //   const publicCopyToastText = page.getByText('Public Key was copied to the clipboard');
-  //   const privateCopyToastText = page.getByText('Private Key was copied to the clipboard');
-
-  //   await publicCopyToastText.waitFor();
-  //   await privateCopyToastText.waitFor();
-
-  //   //Download Key Toast Text
-  //   await page.getByRole('button', { name: 'Download Public Key' }).click();
-  //   await page.getByRole('button', { name: 'Download Private Key' }).click();
-
-  //   const publicDownloadToastText = page.getByText('Public Key is Downloaded');
-  //   const privateDownloadToastText = page.getByText('Private Key is Downloaded');
-
-  //   await publicDownloadToastText.waitFor();
-  //   await privateDownloadToastText.waitFor();
-
-  //   //Text Titles
-  //   const publicKey = page.getByRole('heading', { name: 'Public Key' });
-  //   const privateKey = page.getByRole('heading', { name: 'Private Key' });
-
-  //   await expect(publicKey).toContainText('Public Key');
-  //   await expect(privateKey).toContainText('Private Key');
-  // });
 });
