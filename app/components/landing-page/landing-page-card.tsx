584d8eefe8b9bbdde7e32f689a9723f9507de718
David Humphrey <david.andrew.humphrey@gmail.com>
2023-04-05 16:47:54 -0400

    Switch to use Chakra-UI Card for landing page cards

--- app/components/landing-page/landing-page-card.tsx
+++ app/components/landing-page/landing-page-card.tsx
@@ -1,4 +1,13 @@
-import { Button, Flex, Heading, Text, VStack } from '@chakra-ui/react';
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Flex,
+  Heading,
+  Text,
+} from '@chakra-ui/react';
 import { Link } from '@remix-run/react';
 
 interface LandingPageCardProps {
@@ -17,34 +26,31 @@ export default function LandingPageCard({
   instructionsPath,
 }: LandingPageCardProps) {
   return (
-    <VStack
-      height={{ base: 'xs', md: 'sm' }}
-      borderRadius="xl"
-      padding="5"
-      backgroundColor="whitesmoke"
-    >
-      <Heading size={{ base: 'md', md: 'lg' }} color="brand.500" height="100%">
-        {cardName}
-      </Heading>
-
-      <Flex width={{ md: 'xs' }} height={{ sm: 'xl' }} flexDirection="column" gap="4">
-        <Text fontSize={{ base: 'xs', md: 'md' }}>{cardDescription}</Text>
-        {instructionsPath && (
-          <Text>
-            To learn more, see{' '}
-            <Text as={Link} to={instructionsPath} color="brand.500" textDecor="underline">
-              these instructions
+    <Card variant="filled" backgroundColor="whitesmoke" maxWidth="sm" align="center">
+      <CardHeader>
+        <Heading as="h2" size="lg" color="brand.500" height="100%">
+          {cardName}
+        </Heading>
+      </CardHeader>
+      <CardBody>
+        <Flex direction="column" gap={4}>
+          <Text>{cardDescription}</Text>
+          {instructionsPath && (
+            <Text>
+              To learn more, see{' '}
+              <Text as={Link} to={instructionsPath} color="brand.500" textDecor="underline">
+                these instructions
+              </Text>
+              .
             </Text>
-            .
-          </Text>
-        )}
-      </Flex>
-
-      <Flex width="100%" justifyContent="center" height="100%" alignItems="flex-end">
+          )}
+        </Flex>
+      </CardBody>
+      <CardFooter>
         <Button as={Link} to={{ pathname: path }} size={{ base: 'xs', xs: 'sm', md: 'md' }}>
           {pathName}
         </Button>
-      </Flex>
-    </VStack>
+      </CardFooter>
+    </Card>
   );
 }
