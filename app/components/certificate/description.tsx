e49c185e2eea392bee112238ee29e71712b9cd94
Taimoor D <50856799+SerpentBytes@users.noreply.github.com>
2023-04-10 19:57:43 -0400

    Make 'Renew' button responsive (#561)
    * Make 'Renew' button responsive
    * Use wrap and wrap item
    * fix: ESLint warning

--- app/components/certificate/description.tsx
+++ app/components/certificate/description.tsx
@@ -1,6 +1,6 @@
 import { RepeatIcon } from '@chakra-ui/icons';
 import { Form } from '@remix-run/react';
-import { Flex, Text, HStack, Button, Stat, StatLabel, StatNumber } from '@chakra-ui/react';
+import { Flex, Text, Button, Stat, StatLabel, StatNumber, Wrap, WrapItem } from '@chakra-ui/react';
 
 interface DescriptionSectionProps {
   certRequested: boolean;
@@ -20,29 +20,26 @@ export default function DescriptionSection({
   return (
     <Flex flexDirection="column" gap="3" marginTop={{ base: 1, md: 5 }} fontSize="md">
       <Text maxW={600}>{description}</Text>
-      {certRequested && (
-        <HStack gap="6" marginTop="2">
-          {!!validFromFormatted && !!validToFormatted && (
-            <>
-              <Stat backgroundColor="whitesmoke" maxW={200} px={5} py={3} borderRadius={8}>
-                <StatLabel>Created On</StatLabel>
-                <StatNumber>{validFromFormatted}</StatNumber>
-              </Stat>
-              <Stat backgroundColor="whitesmoke" maxW={200} px={5} py={3} borderRadius={8}>
-                <StatLabel>Expires On</StatLabel>
-                <StatNumber>{validToFormatted}</StatNumber>
-              </Stat>
-
-              <Flex justifyContent="flex-end">
-                <Form method="post">
-                  <Button type="submit" rightIcon={<RepeatIcon />} isDisabled={!isRenewable}>
-                    Renew Certificate
-                  </Button>
-                </Form>
-              </Flex>
-            </>
-          )}
-        </HStack>
+      {certRequested && !!validFromFormatted && !!validToFormatted && (
+        <Wrap align="center">
+          <Stat backgroundColor="whitesmoke" maxW={200} px={5} py={3} borderRadius={8}>
+            <StatLabel>Created On</StatLabel>
+            <StatNumber>{validFromFormatted}</StatNumber>
+          </Stat>
+          <Stat backgroundColor="whitesmoke" maxW={200} px={5} py={3} borderRadius={8}>
+            <StatLabel>Expires On</StatLabel>
+            <StatNumber>{validToFormatted}</StatNumber>
+          </Stat>
+          <WrapItem>
+            <Flex justifyContent="flex-end">
+              <Form method="post">
+                <Button type="submit" rightIcon={<RepeatIcon />} isDisabled={!isRenewable}>
+                  Renew Certificate
+                </Button>
+              </Form>
+            </Flex>
+          </WrapItem>
+        </Wrap>
       )}
     </Flex>
   );
