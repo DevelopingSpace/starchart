fc2a0cf978fd7f9ff58a265ed569d1b902ed0543
TD <50856799+SerpentBytes@users.noreply.github.com>
2023-04-10 18:35:58 -0400

    Use wrap and wrap item

--- app/components/certificate/description.tsx
+++ app/components/certificate/description.tsx
@@ -8,6 +8,7 @@ import {
   Stat,
   StatLabel,
   StatNumber,
+  Wrap,
   WrapItem,
 } from '@chakra-ui/react';
 
@@ -29,30 +30,26 @@ export default function DescriptionSection({
   return (
     <Flex flexDirection="column" gap="3" marginTop={{ base: 1, md: 5 }} fontSize="md">
       <Text maxW={600}>{description}</Text>
-      {certRequested && (
-        <HStack gap="6" marginTop="2" flexWrap="wrap">
-          {!!validFromFormatted && !!validToFormatted && (
-            <>
-              <Stat backgroundColor="whitesmoke" maxW={200} px={5} py={3} borderRadius={8}>
-                <StatLabel>Created On</StatLabel>
-                <StatNumber>{validFromFormatted}</StatNumber>
-              </Stat>
-              <Stat backgroundColor="whitesmoke" maxW={200} px={5} py={3} borderRadius={8}>
-                <StatLabel>Expires On</StatLabel>
-                <StatNumber>{validToFormatted}</StatNumber>
-              </Stat>
-              <WrapItem>
-                <Flex justifyContent="flex-end">
-                  <Form method="post">
-                    <Button type="submit" rightIcon={<RepeatIcon />} isDisabled={!isRenewable}>
-                      Renew Certificate
-                    </Button>
-                  </Form>
-                </Flex>
-              </WrapItem>
-            </>
-          )}
-        </HStack>
+      {certRequested && !!validFromFormatted && !!validToFormatted && (
+        <Wrap align="center">
+          <Stat backgroundColor="whitesmoke" maxW={200} px={5} py={3} borderRadius={8}>
+            <StatLabel>Created On</StatLabel>
+            <StatNumber>{validFromFormatted}</StatNumber>
+          </Stat>
+          <Stat backgroundColor="whitesmoke" maxW={200} px={5} py={3} borderRadius={8}>
+            <StatLabel>Expires On</StatLabel>
+            <StatNumber>{validToFormatted}</StatNumber>
+          </Stat>
+          <WrapItem>
+            <Flex justifyContent="flex-end">
+              <Form method="post">
+                <Button type="submit" rightIcon={<RepeatIcon />} isDisabled={!isRenewable}>
+                  Renew Certificate
+                </Button>
+              </Form>
+            </Flex>
+          </WrapItem>
+        </Wrap>
       )}
     </Flex>
   );
