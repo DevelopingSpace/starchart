4343ac8648785f3ff8b7072ddb850d03c3237a49
Eakampreet <eakam1007@gmail.com>
2023-04-13 13:46:10 -0400

    Fix local e2e workers to 1 to prevent race conditions

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ const config: PlaywrightTestConfig = {
    */
   retries: process.env.CI ? 2 : 1,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
