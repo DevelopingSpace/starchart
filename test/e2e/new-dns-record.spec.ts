2e3614ea73849830c24c32947017235ddcb01542
Eakampreet <eakam1007@gmail.com>
2023-04-13 14:03:20 -0400

    Check for all record valid types

--- test/e2e/new-dns-record.spec.ts
+++ test/e2e/new-dns-record.spec.ts
@@ -1,11 +1,10 @@
 import { test, expect } from '@playwright/test';
-import dayjs from 'dayjs';
 
 import type { Page } from '@playwright/test';
+import type { DnsRecord } from '@prisma/client';
 
 import { loggedInAsUser } from './utils';
 import { prisma } from '../../app/db.server';
-import type { DnsRecord } from '@prisma/client';
 import { DnsRecordType } from '@prisma/client';
 
 test.describe('not authenticated', () => {
@@ -43,6 +42,10 @@ test.describe('authenticated as user', () => {
     page: Page
   ) => {
     return test.step('validate DNS Record data', async () => {
+      // Navigate to edit page to check for values
+      const dnsRecordRow = page.locator('table tr').last();
+      await dnsRecordRow.getByRole('button', { name: 'Edit DNS record' }).click();
+
       await expect(page.getByLabel('Name*')).toHaveValue(dnsRecord.subdomain);
       await expect(page.getByRole('combobox', { name: 'Type' })).toHaveValue(dnsRecord.type);
       await expect(page.getByLabel('Value*')).toHaveValue(dnsRecord.value);
@@ -53,56 +56,61 @@ test.describe('authenticated as user', () => {
   };
 
   test.describe('accepts valid values based on type', () => {
-    test.afterEach(async () => {
-      await prisma.dnsRecord.deleteMany();
-    });
+    const validValueTypeMappings = [
+      { type: DnsRecordType.A, value: '192.168.1.1' },
+      { type: DnsRecordType.AAAA, value: '2001:0db8:85a3:0000:0000:8a2e:0370:7334' },
+      { type: DnsRecordType.CNAME, value: 'test.domain.com' },
+      { type: DnsRecordType.TXT, value: 'starchart' },
+    ];
 
-    test.beforeEach(async ({ page }) => {
-      await page.goto('/dns-records/new');
-    });
+    for (const mapping of validValueTypeMappings) {
+      test.describe(`when type is ${mapping.type} Record`, () => {
+        test.afterEach(async () => {
+          await prisma.dnsRecord.deleteMany();
+        });
 
-    test('when only required fields are filled', async ({ page }) => {
-      const dnsRecord = {
-        subdomain: 'test1-required',
-        type: DnsRecordType.A,
-        value: '192.168.1.1',
-      };
+        test('when only required fields are filled', async ({ page }) => {
+          const dnsRecord = {
+            subdomain: 'test',
+            type: mapping.type,
+            value: mapping.value,
+          };
 
-      await fillDnsRecordFormStep(dnsRecord, page);
-      // Submit form
-      await page.getByRole('button', { name: 'Create' }).click();
+          await fillDnsRecordFormStep(dnsRecord, page);
 
-      // Check if we get redirected to the dns records table
-      await expect(page).toHaveURL('/dns-records');
+          // Submit form
+          await page.getByRole('button', { name: 'Create' }).click();
 
-      // Check if the dns record was created correctly
-      const dnsRecordRow = page.locator('table tr').last();
-      await dnsRecordRow.getByRole('button', { name: 'Edit DNS record' }).click();
-      await checkDnsRecordStep(dnsRecord, page);
-    });
+          // Check if we get redirected to the dns records table
+          await expect(page).toHaveURL('/dns-records');
 
-    test('when all fields are filled', async ({ page }) => {
-      const dnsRecord = {
-        subdomain: 'test2-all',
-        type: DnsRecordType.A,
-        value: '192.168.1.1',
-        ports: 'port1, port2',
-        course: 'test course',
-        description: 'test description',
-      };
+          // Check if the dns record was created correctly
+          await checkDnsRecordStep(dnsRecord, page);
+        });
 
-      await fillDnsRecordFormStep(dnsRecord, page);
-      // Submit form
-      await page.getByRole('button', { name: 'Create' }).click();
+        test('when all fields are filled', async ({ page }) => {
+          const dnsRecord = {
+            subdomain: 'test',
+            type: mapping.type,
+            value: mapping.value,
+            ports: 'port1, port2',
+            course: 'test course',
+            description: 'test description',
+          };
 
-      // Check if we get redirected to the dns records table
-      await expect(page).toHaveURL('/dns-records');
+          await fillDnsRecordFormStep(dnsRecord, page);
 
-      // Check if the dns record was created correctly
-      const dnsRecordRow = page.locator('table tr').last();
-      await dnsRecordRow.getByRole('button', { name: 'Edit DNS record' }).click();
-      await checkDnsRecordStep(dnsRecord, page);
-    });
+          // Submit form
+          await page.getByRole('button', { name: 'Create' }).click();
+
+          // Check if we get redirected to the dns records table
+          await expect(page).toHaveURL('/dns-records');
+
+          // Check if the dns record was created correctly
+          await checkDnsRecordStep(dnsRecord, page);
+        });
+      });
+    }
   });
 
   test.describe('rejects invalid values based on type', () => {
@@ -112,8 +120,7 @@ test.describe('authenticated as user', () => {
       { type: DnsRecordType.CNAME, value: '192.168.1.1' },
       { type: DnsRecordType.TXT, value: '' },
     ];
-    for (let index = 0; index < invalidValueTypeMappings.length; index++) {
-      const mapping = invalidValueTypeMappings[index];
+    for (const mapping of invalidValueTypeMappings) {
       test(`when type is ${mapping.type} Record`, async ({ page }) => {
         const dnsRecord = {
           subdomain: 'test',
