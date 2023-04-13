33731189d9ccd2fffe5e1c311372615ba2ee16fb
Eakampreet <eakam1007@gmail.com>
2023-04-13 01:41:33 -0400

    Refactor tests by moving common steps to functions

--- test/e2e/new-dns-record.spec.ts
+++ test/e2e/new-dns-record.spec.ts
@@ -1,6 +1,8 @@
 import { test, expect } from '@playwright/test';
 import dayjs from 'dayjs';
 
+import type { Page } from '@playwright/test';
+
 import { loggedInAsUser } from './utils';
 import { prisma } from '../../app/db.server';
 
@@ -18,63 +20,93 @@ test.describe('authenticated as user', () => {
     await page.goto('/dns-records/new');
   });
 
-  test.describe('creates a DNS Record and redirects to edit DNS Record page', () => {
-    /**
-     * Clean up after all tests are run
-     * For some weird reason using afterEach to delete the one record doesn't work
-     * The test is run before the records are deleted
-     * Thus, we are using different values for the fields for the tests
-     */
-    test.afterAll(async () => {
-      await prisma.dnsRecord.deleteMany();
-    });
-
-    test('when only required fields are filled', async ({ page }) => {
-      await page.getByLabel('Name*').fill('test1-required');
-      await page.getByRole('combobox', { name: 'Type' }).selectOption('A');
-      await page.getByLabel('Value*').fill('192.168.1.1');
-      await page.getByRole('button', { name: 'Create' }).click();
-
-      // Check if we get redirected to the dns records table
-      await expect(page).toHaveURL('/dns-records');
-      // Check if the dns record was created correctly
-      const dnsRecordRow = page.locator('table tr').nth(1); // First row is the header
-      await expect(dnsRecordRow.locator('td').first()).toContainText(
-        'test1-required.user1.starchart.com'
-      );
-      await expect(dnsRecordRow.locator('td').nth(1)).toContainText('A');
-      await expect(dnsRecordRow.locator('td').nth(2)).toContainText('192.168.1.1');
-      // Check the expiration date
-      const expectedDate = dayjs().add(6, 'month').format('MM/DD/YYYY');
-      await expect(dnsRecordRow.locator('td').nth(3)).toContainText(expectedDate);
-    });
-
-    test('when all fields are filled', async ({ page }) => {
-      await page.getByLabel('Name*').fill('test2-all');
-      await page.getByRole('combobox', { name: 'Type' }).selectOption('A');
-      await page.getByLabel('Value*').fill('192.168.1.1');
-      await page.getByLabel('Ports').fill('port1, port2');
-      await page.getByLabel('Course').fill('test course');
-      await page.getByLabel('Description').fill('test description');
-      await page.getByRole('button', { name: 'Create' }).click();
-
-      // Check if we get redirected to the dns records table
-      await expect(page).toHaveURL('/dns-records');
-      // Check if the dns record was created correctly
-      const dnsRecordRow = page.locator('table tr').last();
-      await expect(dnsRecordRow.locator('td').first()).toContainText(
-        'test2-all.user1.starchart.com'
-      );
-      await expect(dnsRecordRow.locator('td').nth(1)).toContainText('A');
-      await expect(dnsRecordRow.locator('td').nth(2)).toContainText('192.168.1.1');
-      // Check the expiration date
-      const expectedDate = dayjs().add(6, 'month').format('MM/DD/YYYY');
-      await expect(dnsRecordRow.locator('td').nth(3)).toContainText(expectedDate);
-    });
+  /**
+   * Clean up after all tests are run
+   * For some weird reason using afterEach to delete the one record doesn't work
+   * The test is run before the records are deleted
+   * Thus, we are using different values for the fields for the tests
+   */
+  test.afterAll(async () => {
+    await prisma.dnsRecord.deleteMany();
   });
 
-  test('does not create DNS record if required fields are empty', async ({ page }) => {
+  interface DnsRecordData {
+    name: string;
+    type: string;
+    value: string;
+    ports: string;
+    course: string;
+    description: string;
+  }
+
+  const fillDnsRecordFormStep = (dnsRecord: DnsRecordData, page: Page) => {
+    return test.step('fill DNS Record form', async () => {
+      await page.getByLabel('Name*').fill(dnsRecord.name);
+      await page.getByRole('combobox', { name: 'Type' }).selectOption(dnsRecord.type);
+      await page.getByLabel('Value*').fill(dnsRecord.value);
+      await page.getByLabel('Ports').fill(dnsRecord.ports);
+      await page.getByLabel('Course').fill(dnsRecord.course);
+      await page.getByLabel('Description').fill(dnsRecord.description);
+    });
+  };
+
+  const checkDnsRecordStep = (dnsRecord: DnsRecordData, page: Page) => {
+    return test.step('validate DNS Record data', async () => {
+      await expect(page.getByLabel('Name*')).toHaveValue(dnsRecord.name);
+      await expect(page.getByRole('combobox', { name: 'Type' })).toHaveValue(dnsRecord.type);
+      await expect(page.getByLabel('Value*')).toHaveValue(dnsRecord.value);
+    });
+  };
+
+  test('when only required fields are filled', async ({ page }) => {
+    const dnsRecord: DnsRecordData = {
+      name: 'test1-required',
+      type: 'A',
+      value: '192.168.1.1',
+      ports: '',
+      course: '',
+      description: '',
+    };
+
+    await fillDnsRecordFormStep(dnsRecord, page);
+    // Submit form
     await page.getByRole('button', { name: 'Create' }).click();
+
+    // Check if we get redirected to the dns records table
+    await expect(page).toHaveURL('/dns-records');
+
+    // Check if the dns record was created correctly
+    const dnsRecordRow = page.locator('table tr').nth(1); // First row is the header
+    await dnsRecordRow.getByRole('button', { name: 'Edit DNS record' }).click();
+    await checkDnsRecordStep(dnsRecord, page);
+  });
+
+  test('when all fields are filled', async ({ page }) => {
+    const dnsRecord: DnsRecordData = {
+      name: 'test2-all',
+      type: 'A',
+      value: '192.168.1.1',
+      ports: 'port1, port2',
+      course: 'test course',
+      description: 'test description',
+    };
+
+    await fillDnsRecordFormStep(dnsRecord, page);
+    // Submit form
+    await page.getByRole('button', { name: 'Create' }).click();
+
+    // Check if we get redirected to the dns records table
+    await expect(page).toHaveURL('/dns-records');
+
+    // Check if the dns record was created correctly
+    const dnsRecordRow = page.locator('table tr').last();
+    await dnsRecordRow.getByRole('button', { name: 'Edit DNS record' }).click();
+    await checkDnsRecordStep(dnsRecord, page);
+  });
+
+  test('when required fields are empty', async ({ page }) => {
+    await page.getByRole('button', { name: 'Create' }).click();
+    // We expect to remain on the same page
     await expect(page).toHaveURL('/dns-records/new');
   });
 });
