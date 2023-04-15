7f61405dcce0f2a1108984aeefea68718bbcd431
Eakampreet <eakam1007@gmail.com>
2023-04-15 11:34:31 -0400

    Use v2 meta function

--- remix.config.js
+++ remix.config.js
@@ -5,8 +5,9 @@ module.exports = {
   cacheDirectory: './node_modules/.cache/remix',
   ignoredRouteFiles: ['**/.*', '**/*.css', '**/*.test.{js,jsx,ts,tsx}'],
   future: {
-    v2_routeConvention: true,
-    v2_normalizeFormMethod: true,
     v2_errorBoundary: true,
+    v2_meta: true,
+    v2_normalizeFormMethod: true,
+    v2_routeConvention: true,
   },
 };
