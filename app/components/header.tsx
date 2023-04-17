4e483e100d0a68f13fc0a4b892cd74c9a733c92e
Ririio <xRiriio@gmail.com>
2023-04-17 03:48:28 -0400

    making username and avatar clickable

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -96,24 +96,25 @@ export default function Header() {
         </Heading>
       </Link>
 
-      <Flex justifyContent="flex-end" alignItems="center" color="white" gap="5" width="100%">
-        <Hide below="lg">
-          <Text id="header-user">
-            {user.username === originalUser.username
-              ? user?.username
-              : `Impersonating: ${user?.username}`}
-          </Text>
-        </Hide>
-
+      <Flex justifyContent="flex-end" alignItems="center" color="white" width="100%">
         <Menu>
           <MenuButton
             as={IconButton}
             icon={
-              user.username !== originalUser.username ? (
-                <FaTheaterMasks size="35" />
-              ) : (
-                <Avatar bg="brand.500" showBorder={true} borderColor="white" size="sm" />
-              )
+              <HStack gap="5">
+                <Hide below="lg">
+                  <Text id="header-user">
+                    {user.username === originalUser.username
+                      ? user?.username
+                      : `Impersonating: ${user?.username}`}
+                  </Text>
+                </Hide>
+                {user.username !== originalUser.username ? (
+                  <FaTheaterMasks size="35" />
+                ) : (
+                  <Avatar bg="brand.500" showBorder={true} borderColor="white" size="sm" />
+                )}
+              </HStack>
             }
             style={{ backgroundColor: 'transparent' }}
           />
