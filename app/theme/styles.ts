b587bcc935cf1b0c028e2845f52789b7cde8ccfd
David Humphrey <david.andrew.humphrey@gmail.com>
2023-04-21 17:59:16 -0400

    Use Chakra theme to style root elements

---
+++ app/theme/styles.ts
@@ -0,0 +1,14 @@
+// We want html and body to occupy the full window height for vertical centering
+const styles = {
+  global: {
+    html: {
+      height: '100%',
+    },
+    body: {
+      height: '100%',
+      minHeight: '100%',
+    },
+  },
+};
+
+export default styles;
