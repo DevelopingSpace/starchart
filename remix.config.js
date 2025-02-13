e41842c056eb189fdfca35699909b0c0ab04115a
Uday R <82779467+uday-rana@users.noreply.github.com>
2025-02-13 05:54:00 -0500

    Set serverModuleFormat to cjs

--- remix.config.js
+++ remix.config.js
@@ -4,6 +4,7 @@
 module.exports = {
   cacheDirectory: './node_modules/.cache/remix',
   ignoredRouteFiles: ['**/.*', '**/*.css', '**/*.test.{js,jsx,ts,tsx}'],
+  serverModuleFormat: 'cjs',
   future: {
     v2_dev: true,
     v2_errorBoundary: true,
