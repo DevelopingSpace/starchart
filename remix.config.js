b0674d6b5334ecbada55456e244a866933b70916
Uday R <82779467+uday-rana@users.noreply.github.com>
2025-02-13 07:37:35 -0500

    Remove future flags

--- remix.config.js
+++ remix.config.js
@@ -5,12 +5,4 @@ module.exports = {
   cacheDirectory: './node_modules/.cache/remix',
   ignoredRouteFiles: ['**/.*', '**/*.css', '**/*.test.{js,jsx,ts,tsx}'],
   serverModuleFormat: 'cjs',
-  future: {
-    v2_dev: true,
-    v2_errorBoundary: true,
-    v2_headers: true,
-    v2_meta: true,
-    v2_normalizeFormMethod: true,
-    v2_routeConvention: true,
-  },
 };
