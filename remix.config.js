c5d1554b2d1ed3f61aa894d8395aef105820272d
Uday R <82779467+uday-rana@users.noreply.github.com>
2025-02-13 04:37:09 -0500

    Enable v2_dev feature flag

--- remix.config.js
+++ remix.config.js
@@ -5,6 +5,7 @@ module.exports = {
   cacheDirectory: './node_modules/.cache/remix',
   ignoredRouteFiles: ['**/.*', '**/*.css', '**/*.test.{js,jsx,ts,tsx}'],
   future: {
+    v2_dev: true,
     v2_errorBoundary: true,
     v2_meta: true,
     v2_normalizeFormMethod: true,
