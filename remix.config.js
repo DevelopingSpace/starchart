2ade71b45397b26ed87755c43026b9e487e48724
Uday R <82779467+uday-rana@users.noreply.github.com>
2025-02-13 05:53:00 -0500

    Enable v2_headers feature flag

--- remix.config.js
+++ remix.config.js
@@ -7,6 +7,7 @@ module.exports = {
   future: {
     v2_dev: true,
     v2_errorBoundary: true,
+    v2_headers: true,
     v2_meta: true,
     v2_normalizeFormMethod: true,
     v2_routeConvention: true,
