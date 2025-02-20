f34879ee49201bb0a641a15a8ab3848bb1f9da0b
Uday Rana <82779467+uday-rana@users.noreply.github.com>
2025-02-20 16:19:55 -0500

    Migrate custom server

--- server.ts
+++ server.ts
@@ -3,7 +3,7 @@ import path from 'path';
 import express from 'express';
 import compression from 'compression';
 import { createRequestHandler } from '@remix-run/express';
-import { broadcastDevReady } from '@remix-run/node';
+import { installGlobals, ServerBuild } from '@remix-run/node';
 import gracefulShutdown from 'http-graceful-shutdown';
 import helmet from 'helmet';
 import cors from 'cors';
@@ -13,73 +13,101 @@ import * as services from 'services';
 
 import type { Request, Response } from 'express';
 
-const MODE = process.env.NODE_ENV;
-const app = express();
+installGlobals();
 
-app.use((_req, res, next) => {
-  res.locals.nonce = crypto.randomBytes(16).toString('hex');
-  next();
-});
+(async () => {
+  const viteDevServer =
+    process.env.NODE_ENV === 'production'
+      ? undefined
+      : await import('vite').then((vite) =>
+          vite.createServer({
+            server: { middlewareMode: true },
+          })
+        );
 
-app.use(
-  helmet({
-    contentSecurityPolicy: {
-      useDefaults: true,
-      directives: {
-        // Expect a nonce on scripts
-        scriptSrc: ["'self'", (_req, res) => `'nonce-${(res as Response).locals.nonce}'`],
-        // Allow live reload to work over a web socket in development
-        connectSrc: MODE === 'production' ? ["'self'"] : ["'self'", 'ws:'],
-        // Don't force https unless in production
-        upgradeInsecureRequests: MODE === 'production' ? [] : null,
-      },
-    },
-  })
-);
+  const MODE = process.env.NODE_ENV;
+  const app = express();
 
-app.use(cors());
-
-app.use((req, res, next) => {
-  // /clean-urls/ -> /clean-urls
-  if (req.path.endsWith('/') && req.path.length > 1) {
-    const query = req.url.slice(req.path.length);
-    const safepath = req.path.slice(0, -1).replace(/\/+/g, '/');
-    res.redirect(301, safepath + query);
-    return;
-  }
-  next();
-});
-
-app.use(compression());
-
-// Remix fingerprints its assets so we can cache forever.
-app.use('/build', express.static('public/build', { immutable: true, maxAge: '1y' }));
-
-// Everything else (like favicon.ico) is cached for an hour. You may want to be
-// more aggressive with this caching.
-app.use(express.static('public', { maxAge: '1h' }));
-
-const BUILD_DIR = path.join(process.cwd(), 'build');
-const build = require(BUILD_DIR);
-
-// Pass the nonce we're setting in the CSP headers down to the Remix Loader/Action functions
-const getLoadContext = (_req: Request, res: Response) => ({ nonce: res.locals.nonce });
-
-app.all('*', createRequestHandler({ build, getLoadContext }));
-
-const port = process.env.PORT || 8080;
-
-const server = app.listen(port, () => {
-  // start the various background jobs we run (reconciler, expire records, etc)
-  services.init().then(() => {
-    logger.info(`✅ app ready: http://localhost:${port}`);
+  app.use((_req, res, next) => {
+    res.locals.nonce = crypto.randomBytes(16).toString('hex');
+    next();
   });
 
-  if (process.env.NODE_ENV === 'development') {
-    broadcastDevReady(build);
-  }
-});
+  app.use(
+    helmet({
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          // Expect a nonce on scripts
+          scriptSrc: ["'self'", (_req, res) => `'nonce-${(res as Response).locals.nonce}'`],
+          // Allow live reload to work over a web socket in development
+          connectSrc: MODE === 'production' ? ["'self'"] : ["'self'", 'ws:'],
+          // Don't force https unless in production
+          upgradeInsecureRequests: MODE === 'production' ? [] : null,
+        },
+      },
+    })
+  );
 
-gracefulShutdown(server, {
-  development: process.env.NODE_ENV !== 'production',
-});
+  app.use(cors());
+
+  app.use((req, res, next) => {
+    // /clean-urls/ -> /clean-urls
+    if (req.path.endsWith('/') && req.path.length > 1) {
+      const query = req.url.slice(req.path.length);
+      const safepath = req.path.slice(0, -1).replace(/\/+/g, '/');
+      res.redirect(301, safepath + query);
+      return;
+    }
+    next();
+  });
+
+  app.use(compression());
+
+  // handle asset requests
+  if (viteDevServer) {
+    app.use(viteDevServer.middlewares);
+  } else {
+    // Remix fingerprints its assets so we can cache forever.
+    app.use('/build', express.static('public/build', { immutable: true, maxAge: '1y' }));
+  }
+
+  // Everything else (like favicon.ico) is cached for an hour. You may want to be
+  // more aggressive with this caching.
+  app.use(express.static('public', { maxAge: '1h' }));
+
+  // Pass the nonce we're setting in the CSP headers down to the Remix Loader/Action functions
+  const getLoadContext = (_req: Request, res: Response) => ({ nonce: res.locals.nonce });
+
+  // handle SSR requests
+  // we need to do these assertions because viteDevServer.ssrLoadModule
+  // returns a Promise<Record<string,any>>
+  app.all(
+    '*',
+    createRequestHandler({
+      build: viteDevServer
+        ? async () => {
+            const mod = await viteDevServer.ssrLoadModule('virtual:remix/server-build');
+            return mod as unknown as ServerBuild;
+          }
+        : async () => {
+            const mod = await import('./build/server/index.js');
+            return mod as unknown as ServerBuild;
+          },
+      getLoadContext,
+    })
+  );
+
+  const port = process.env.PORT || 8080;
+
+  const server = app.listen(port, () => {
+    // start the various background jobs we run (reconciler, expire records, etc)
+    services.init().then(() => {
+      logger.info(`✅ app ready: http://localhost:${port}`);
+    });
+  });
+
+  gracefulShutdown(server, {
+    development: process.env.NODE_ENV !== 'production',
+  });
+})();
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,22 @@
 import { vitePlugin as remix } from '@remix-run/dev';
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { cjsInterop } from 'vite-plugin-cjs-interop';
 
 export default defineConfig({
   plugins: [
-    remix({
-      ignoredRouteFiles: ['**/.*', '**/*.css', '**/*.test.{js,jsx,ts,tsx}'],
-      serverModuleFormat: 'cjs',
+    !process.env.VITEST &&
+      remix({
+        ignoredRouteFiles: ['**/.*', '**/*.css', '**/*.test.{js,jsx,ts,tsx}'],
+        serverModuleFormat: 'cjs',
+      }),
+    tsconfigPaths(),
+    // https://remix.run/docs/en/main/guides/vite#esm--cjs
+    cjsInterop({
+      dependencies: ['@emotion/cache'],
     }),
   ],
+  optimizeDeps: {
+    include: ['@emotion/cache'],
+  },
 });
