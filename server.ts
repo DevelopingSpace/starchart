664b7f8871ca33eec0e29678f6dde8a0716b1ddb
Uday Rana <82779467+uday-rana@users.noreply.github.com>
2025-04-02 21:54:29 -0400

    fix eslint ignore rule

--- server.ts
+++ server.ts
@@ -92,8 +92,7 @@ installGlobals();
         : async () => {
             // Need to disable these rules here
             // because the imported file needs to be generated first
-            // eslint-disable-next-line import/no-unresolved
-            // @ts-ignore
+            // @ts-ignore  eslint-disable-next-line import/no-unresolved
             const mod = await import('./build/server/index.js');
             return mod as unknown as ServerBuild;
           },
