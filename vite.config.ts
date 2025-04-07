8f73ff667eaea59a7915ba4ac6b07b879b449230
Uday Rana <82779467+uday-rana@users.noreply.github.com>
2025-04-07 01:22:58 -0400

    move build step into vite config buildEnd

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,32 @@ import { vitePlugin as remix } from '@remix-run/dev';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { cjsInterop } from 'vite-plugin-cjs-interop';
+import esbuild from 'esbuild';
 
 export default defineConfig({
   plugins: [
     !process.env.VITEST &&
       remix({
         ignoredRouteFiles: ['**/.*', '**/*.css', '**/*.test.{js,jsx,ts,tsx}'],
+        serverBuildFile: 'remix.js',
+        buildEnd: async () => {
+          await esbuild
+            .build({
+              alias: { '~': './app' },
+              outfile: './build/server/index.js',
+              entryPoints: ['./server.ts'],
+              external: ['./build/server/*'],
+              platform: 'node',
+              format: 'esm',
+              packages: 'external',
+              bundle: true,
+              logLevel: 'info',
+            })
+            .catch((error: unknown) => {
+              console.error('Error building server:', error);
+              process.exit(1);
+            });
+        },
       }),
     tsconfigPaths(),
     // https://remix.run/docs/en/main/guides/vite#esm--cjs
