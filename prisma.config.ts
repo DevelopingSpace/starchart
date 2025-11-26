8d10242a7b5c9638bfacfc7e5c65b7952daefd0b
David Humphrey <david.andrew.humphrey@gmail.com>
2025-11-26 11:33:02 -0500

    Don't use prisma's env() for loading DATABASE_URL

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import { defineConfig, env } from 'prisma/config';
+import { defineConfig } from 'prisma/config';
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
@@ -8,6 +8,7 @@ export default defineConfig({
     seed: 'tsx --require tsconfig-paths/register prisma/seed.ts',
   },
   datasource: {
-    url: env('DATABASE_URL') || 'mysql://starchart:starchart_password@127.0.0.1:3306/starchart',
+    url:
+      process.env.DATABASE_URL || 'mysql://starchart:starchart_password@127.0.0.1:3306/starchart',
   },
 });
